d2fa73fe9b9e950c46b725581eccecf19bd1e58d
NNADIHENRY <nnadihenry92@gmail.com>
2018-11-09 14:35:28 +0000

    second commit

--- app/src/main/java/com/example/henry/ighubchurchapp/HomePage.java
+++ app/src/main/java/com/example/henry/ighubchurchapp/HomePage.java
@@ -1,13 +1,97 @@
 package com.example.henry.ighubchurchapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements MenuItem.OnMenuItemClickListener{
+
+    Button showMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        TableLayout tv =  this.findViewById(R.id.table);
+        tv.setSelected(true);  // Set focus to the textview
+        showMenu = findViewById(R.id.show_dropdown_menu);
+        showMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu dropDownMenu = new PopupMenu(getApplicationContext(), showMenu);
+                dropDownMenu.getMenuInflater().inflate(R.menu.drop_down_menu, dropDownMenu.getMenu());
+                showMenu.setText("CHURCH MENU");
+                dropDownMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        String a = menuItem.getTitle().toString();
+                        Toast.makeText(getApplicationContext(), "You have clicked " + menuItem.getTitle(), Toast.LENGTH_LONG).show();
+                        return true;
+                    }
+                });
+                dropDownMenu.show();
+            }
+        });
     }
-}
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+
+        return false;
+    }
+    private void inflate(){
+        LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflater.inflate(R.layout.selectregistration, null);
+        // AlertDialog used for pop-Ups
+        AlertDialog.Builder builder = new AlertDialog.Builder(HomePage.this);
+        builder.setView(view);
+
+
+
+        builder.setCancelable(false)
+//                positive button is used to indicate whether to save or update
+                .setPositiveButton("Register",
+                        new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialogBox, int id) {
+
+                            }
+                        })
+                // Used to set Negative button to cancel
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialogBox, int id) {
+                        dialogBox.cancel();
+
+                    }
+                });
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+        alertDialog.getButton(alertDialog.BUTTON_POSITIVE).setOnClickListener
+                (new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                    }
+                });
+
+
+    }
+    }
+
---
+++ app/src/main/java/com/example/henry/ighubchurchapp/Splash.java
@@ -0,0 +1,31 @@
+package com.example.henry.ighubchurchapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Thread sleep = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+
+                }
+                finally {
+                    startActivity(new Intent(Splash.this, HomePage.class));
+
+                }
+            }
+        };
+        sleep.start();
+    }
+}
