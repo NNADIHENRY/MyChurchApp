234a0f2b573ab9f577bfdbd361adf8821169af35
Chidinma-Ogueri <chydynma.co@gmail.com>
2018-11-14 11:32:30 +0100

    Changes made by Chidinma

--- app/src/main/java/com/example/henry/ighubchurchapp/PastorRegistration.java
+++ app/src/main/java/com/example/henry/ighubchurchapp/PastorRegistration.java
@@ -2,6 +2,7 @@ package com.example.henry.ighubchurchapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,8 +10,11 @@ import android.icu.text.SimpleDateFormat;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -19,6 +23,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,7 +50,7 @@ import java.util.regex.Pattern;
         private List<String> myList;  // String list that contains file paths to images
         private GridView gridview;
         private String mCurrentPhotoPath;  // File path to the last image captured
-
+        Button showMenu;
 
         //i created a string for the xml
         private String surname, firstname, othername, email, residential, phone;
@@ -93,6 +99,55 @@ import java.util.regex.Pattern;
                     }
                 }
             });
+            showMenu = findViewById(R.id.show_dropdown_menu);
+            showMenu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    PopupMenu dropDownMenu = new PopupMenu(getApplicationContext(), showMenu);
+                    dropDownMenu.getMenuInflater().inflate(R.menu.drop_down_menu, dropDownMenu.getMenu());
+                    showMenu.setText("...");
+                    dropDownMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+                            boolean me;
+                            if(menuItem.getTitle().toString().matches("Registration") ){
+
+                                showDialog();
+                                me = true;
+
+                            }
+                            else if(menuItem.getTitle().toString().matches("Login") ){
+                                me = true;
+
+                            }
+                            else if(menuItem.getTitle().toString().matches("Pay Tithe/Offering")){
+                                me = true;
+
+
+                            }
+                            else if(menuItem.getTitle().toString().matches( "View Pastors")){
+                                me = true;
+
+                            }
+                            else if(menuItem.getTitle().toString().matches( "Weekly Activity")){
+                                weeklyActivity();
+                                me = true;
+
+                            }
+                            else {
+                                me = false;
+                            }
+                            return me;
+
+
+
+                        }
+                    });
+                    dropDownMenu.show();
+                }
+            });
+
 
 
         }
@@ -167,6 +222,107 @@ import java.util.regex.Pattern;
             }
 
         }
-            }
+        private void weeklyActivity() {
+            // Override active layout
+            LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+            View view = layoutInflater.inflate(R.layout.weekly_activity, null);
+            // AlertDialog used for pop-Ups
+            AlertDialog.Builder builder = new AlertDialog.Builder(PastorRegistration.this);
+            builder.setView(view);
+
+            builder.setCancelable(true)
+//                positive button is used to indicate whether to save or update
+                    .setPositiveButton( "Continue",
+                            new DialogInterface.OnClickListener() {
+
+                                public void onClick(DialogInterface dialogBox, int id) {
+                                }
+                            })
+                    // Used to set Negative button to cancel
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                        public void onClick(DialogInterface dialogBox, int id) {
+                            dialogBox.cancel();
+
+                        }
+                    });
+
+            final AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+            alertDialog.getButton(alertDialog.BUTTON_POSITIVE).setOnClickListener
+                    (new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            alertDialog.dismiss();
+
+                        }
+                    });
+
+
+        }
+        private void showDialog() {
+            // Override active layout
+            LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+            View view = layoutInflater.inflate(R.layout.selectregistration, null);
+            // AlertDialog used for pop-Ups
+            AlertDialog.Builder builder = new AlertDialog.Builder(PastorRegistration.this);
+            builder.setView(view);
+
+            // Used to link or get views in the dialogBox
+            TextView text = view.findViewById(R.id.reg);
+            final RadioButton pastor = view.findViewById(R.id.btnpastor);
+            final RadioButton member = view.findViewById(R.id.btnmember);
+
+            builder.setCancelable(false)
+//                positive button is used to indicate whether to save or update
+                    .setPositiveButton( "Continue",
+                            new DialogInterface.OnClickListener() {
+
+                                public void onClick(DialogInterface dialogBox, int id) {
+                                }
+                            })
+                    // Used to set Negative button to cancel
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                        public void onClick(DialogInterface dialogBox, int id) {
+                            dialogBox.cancel();
+
+                        }
+                    });
+
+            final AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+            alertDialog.getButton(alertDialog.BUTTON_POSITIVE).setOnClickListener
+                    (new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // Show toast message when no text is entered
+
+                            if (pastor.isChecked()==false && member.isChecked()==false){
+
+                                Toast.makeText(PastorRegistration.this, " select registration type ",
+                                        Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                            if(member.isChecked()==true) {
+                                startActivity(new Intent(PastorRegistration.this, MembersRegistration.class));
+                            }else if(pastor.isChecked()==true) {
+                                startActivity(new Intent(PastorRegistration.this, PastorRegistration.class));
+                            }
+                            else {
+                                alertDialog.dismiss();
+                            }
+
+                            // check if user updating note
+
+
+                        }
+                    });
+
+
+        }
+
+
+    }
 
 
